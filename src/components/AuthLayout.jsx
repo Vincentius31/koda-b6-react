288d7009991c667b112d4e222cb4890e83665a58
KODA <vincentius85@gmail.com>
2026-01-22 11:24:27 +0700

    fix: fixing the structure of the AuthLayout component due the image is separate now just in Authlayout and adding styling using tailwindcss

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,12 +1,12 @@
-import AuthImage from "../components/AuthImage.jsx"
+import loginImage from "../assets/img/Rectangle 289.png"
 
 export default function AuthLayout({children}){
     return (
-        <div className="">
-            <div className="">
-                <img src="../assets/img/rectangle 289.png" alt="Image-Login" />
+        <div className="min-h-screen flex">
+            <div className="hidden md:block md:w-1/2">
+                <img src={loginImage} alt="Image-Login" className="h-full w-full object-cover"/>
             </div>
-            <div className="">
+            <div className="flex w-full md: w-1/2  items-center justify-center">
                 {children}
             </div>
         </div>

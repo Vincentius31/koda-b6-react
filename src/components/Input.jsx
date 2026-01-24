7f779d7fe0d5c8fcacfc868d2d53f92a933bd6dd
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-24 14:36:40 +0700

    feat: adding Input component for Input text type without eyelash icon

---
+++ src/components/Input.jsx
@@ -0,0 +1,18 @@
+export default function Input({ label, type, placeholder, icon: Icon }) {
+    return (
+        <div className="form-group mb-5">
+            <label className="block text-sm mb-2">{label}</label>
+            <div className="input-box flex items-center border rounded-md px-3 py-3">
+                {Icon && <Icon className="w-5 mr-3"/>}
+                <input
+                    type={type}
+                    placeholder={placeholder}
+                    className="w-full outline-none border-none"
+                />
+            </div>
+        </div>
+    )
+}
+
+
+

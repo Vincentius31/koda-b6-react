4e3280764a4c043ff891dcfcc7f040c2b3d05185
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-24 15:53:24 +0700

    feat: adding PrimaryButton component for the using in all this page what using orange button

---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+export const PrimaryButton = (btn) => {
+  return (
+    <button
+      type="submit"
+      className="btn-register bg-[#FF8906] w-full py-3 mt-6 bg-primary text-white rounded-md hover:bg-primaryHover transition"
+    >{btn.children}</button>
+  )
+}
+

d026f75a085cdf4262ec70814df4f24a5b893429
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-24 16:02:36 +0700

    fix: the gap issue in this component for the register page

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -2,7 +2,7 @@ import { Eye, KeyRound } from "lucide-react"
 
 export default function Password({ label, placeholder }) {
     return (
-        <div className="form-group mb-5">
+        <div className="form-group">
             <label className="block text-sm mb-2">{label}</label>
             <div className="eye input-box flex items-center border rounded-md px-3 py-3">
                 <KeyRound className="w-5 mr-3"/>

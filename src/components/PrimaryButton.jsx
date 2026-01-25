30b2e990938c54173e7cd1f1bceb7d06d5435bfc
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-25 10:18:47 +0700

    fix: remove the unnesecary className

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -2,7 +2,7 @@ export const PrimaryButton = (btn) => {
   return (
     <button
       type="submit"
-      className="btn-register bg-[#FF8906] w-full py-3 mt-6 bg-primary text-white rounded-md hover:bg-primaryHover transition"
+      className="bg-[#FF8906] w-full py-3 mt-6 bg-primary text-white rounded-md hover:bg-primaryHover transition"
     >{btn.children}</button>
   )
 }

8415053771ae46d3d5cee4179e576909dc2664d4
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-29 00:41:24 +0700

    feat: adding App.jsx for the route

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
 import { createRoot } from "react-dom/client";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import { ForgotPassPage } from "./pages/ForgotPassPage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import App from "./App";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
 
-const H1 = function(){
-    return (<h1>Hello World</h1>)
-}
 
-root.render(<Footer/>)
+root.render(<App/>)

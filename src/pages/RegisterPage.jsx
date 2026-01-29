57a4d74b96fab73f9ce728344a4121e309b148e1
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-29 14:18:41 +0700

    feat: adding register user features using yup, hook-form, and useNavigate

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-// Image Import
+// Image and Icon Import
 import pictureLeft from "../assets/img/Rectangle 289.png"
 import logoBrown from "../assets/img/Logo-Brown.png"
 import logoFacebook from "../assets/img/facebook.png"
@@ -9,9 +9,50 @@ import { Mail, User } from 'lucide-react'
 import Input from "../components/Input";
 import Password from "../components/Password";
 import { PrimaryButton } from "../components/PrimaryButton";
-import { Link } from "react-router-dom";
+
+// Library Import
+import { Link, useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { useForm} from "react-hook-form"
+
+const schema = yup.object({
+  fullname: yup.string().required("Full name wajib diisi"),
+  email: yup.string().email("Email tidak valid").required("Email wajib diisi"),
+  password: yup.string().min(4, "Minimal 4 karakter").required("Password wajib diisi"),
+  confirmPassword: yup.string().oneOf([yup.ref('password')], "Password tidak sama").required("Confirm password wajib diisi"),
+})
 
 export default function RegisterPage() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data) => {
+    const users = JSON.parse(localStorage.getItem("users")) || []
+    const emailExist = users.find(user=> user.email === data.email)
+
+    if(emailExist){
+      alert("Email sudah terdaftar")
+      return
+    }
+
+    users.push({
+      fullName: data.fullName,
+      email: data.email,
+      password: data.password
+    })
+
+    localStorage.setItem("users", JSON.stringify(users))
+    alert("Register berhasil")
+    navigate("/login")
+  }
 
   return (
     <div>
@@ -30,16 +71,39 @@ export default function RegisterPage() {
             <p className="content text-gray-400 mb-6">Fill out the form correctly</p>
 
             {/* Section Form */}
-            <form className="flex flex-col gap-6">
-              <Input label={"Full Name"} type={"text"} placeholder={"Enter Your Full Name"} icon={User} />
-              <Input label={"Email"} type={"email"} placeholder={"Enter Your Email"} icon={Mail} />
-              <Password label={"Password"} placeholder={"Enter Your Password"} />
-              <Password label={"Confirm Password"} placeholder={"Enter Your Password"} />
+            <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
+              <Input 
+                label={"Full Name"} 
+                type={"text"} 
+                placeholder={"Enter Your Full Name"} 
+                icon={User} 
+                register={register("fullname")}
+                error={errors.fullname?.message}
+              />
+              <Input 
+                label={"Email"} 
+                type={"email"} 
+                placeholder={"Enter Your Email"} 
+                icon={Mail}
+                register={register("email")}
+                error={errors.email?.message} 
+              />
+              <Password 
+                label={"Password"} 
+                placeholder={"Enter Your Password"}
+                register={register("password")}
+                error={errors.password?.message} 
+              />
+              <Password 
+                label={"Confirm Password"} 
+                placeholder={"Enter Your Password"}
+                register={register("confirmPassword")}
+                error={errors.confirmPassword?.message}
+              />
+
+              <PrimaryButton>Register</PrimaryButton>
             </form>
-
-            {/* Button Register */}
-            <Link to={"/login"}><PrimaryButton>Register</PrimaryButton></Link>
-
+            
             <p className="login-text text-center text-sm my-4">
               Have An Account?
               <Link to={"/login"} className="text-primary ml-1 hover:underline text-[#FF8906]">Login</Link>

eeffdff071b210f94d070b90f791c364de70f508
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-29 14:10:47 +0700

    feat: adding more props to using hook form and display the error in the field of input

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,6 +1,6 @@
 import { Eye, KeyRound } from "lucide-react"
 
-export default function Password({ label, placeholder }) {
+export default function Password({ label, placeholder, register, error }) {
     return (
         <div className="form-group">
             <label className="block text-sm mb-2">{label}</label>
@@ -10,9 +10,12 @@ export default function Password({ label, placeholder }) {
                     type="password"
                     placeholder={placeholder}
                     className="w-full outline-none border-none"
+                    {...register}
                 />
                 <Eye className="w-5 mr-3"/>
             </div>
+
+            {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
         </div>
     )
 }

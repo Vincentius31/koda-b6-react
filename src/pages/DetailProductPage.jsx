8a0a21d19a239f324f2c664316026a6c669c92be
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-31 13:51:50 +0700

    feat: adding function for pagination function and display data and recommended items by the id for primary key of the data

--- src/pages/DetailProductPage.jsx
+++ src/pages/DetailProductPage.jsx
@@ -1,18 +1,46 @@
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import ProductGallery from "../components/ProductGallery"
-import img1 from "../assets/img/Image 31.png"
-import img2 from "../assets/img/Image 32.png"
-import img3 from "../assets/img/Image 33.png"
-import img4 from "../assets/img/Image 34.png"
 import ProductInfo from "../components/ProductInfo"
 import ProductCard from "../components/ProductCard"
 import imgProductCard from "../assets/img/Image 30.png"
-
-
-
+import { useLocation, useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 export default function DetailProductPage() {
+    const { id } = useParams();
+    const location = useLocation();
+    const [product, setProduct] = useState(location.state?.product || null);
+
+    const [recommended, setRecommended] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const ITEM_PER_PAGE = 3
+
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/Vincentius31/koda-b6-react/refs/heads/main/src/data/menu.json")
+            .then(res => res.json())
+            .then(data => {
+                const found = data.find(item => String(item.id) === id);
+                setProduct(found || null)
+
+                const filtered = data.filter(item => String(item.id) !== id);
+                setRecommended(filtered)
+            })
+            .catch(() => {
+                setProduct(null)
+                setRecommended([])
+            });
+    }, [id]);
+
+    if (!product) {
+        return <p className="p-6">Product not found</p>
+    }
+
+    const startIndex = (currentPage - 1) * ITEM_PER_PAGE
+    const currentItems = recommended.slice(startIndex, startIndex + ITEM_PER_PAGE)
+    const totalPages = Math.ceil(recommended.length / ITEM_PER_PAGE)
+
     return (
         <div>
             <Navbar className="bg-black" />
@@ -20,9 +48,8 @@ export default function DetailProductPage() {
             {/* Product Showcase */}
             <section className="max-w-7xl mx-auto px-4 py-12">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-20">
-                    <ProductGallery img1={img1} img2={img2} img3={img3} img4={img4} />
-                    <ProductInfo name={"Hazelnut Latte"} priceNormal={"IDR 20.000"} priceDiscount={"IDR 10.000"}
-                        descriptionProduct={"Cold brewing is a method of brewing that combines ground coffee and cool water and uses time instead of heat to extract the flavor. It is brewed in small batches and steeped for as long as 48 hours."}
+                    <ProductGallery img1={product.imageDepan} img2={product.imageKedua} img3={product.imageKetiga} img4={product.imageKeempat} />
+                    <ProductInfo name={product.nameProduct} priceNormal={`IDR ${product.priceProduct}`} priceDiscount={`IDR ${product.priceDiscount}`} descriptionProduct={product.description}
                     />
                 </div>
 
@@ -32,36 +59,34 @@ export default function DetailProductPage() {
                     </h2>
 
                     {/* Product List */}
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-300">
-                        <ProductCard name={"Hazelnut Latte"} src={imgProductCard} 
-                        description={"You can explore the menu that we provide with fun and have their own taste and make your day better."} price={"IDR 20.000"}/>
-                        <ProductCard name={"Hazelnut Latte"} src={imgProductCard} 
-                        description={"You can explore the menu that we provide with fun and have their own taste and make your day better."} price={"IDR 20.000"}/>
-                        <ProductCard name={"Hazelnut Latte"} src={imgProductCard} 
-                        description={"You can explore the menu that we provide with fun and have their own taste and make your day better."} price={"IDR 20.000"}/>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-300">
+                        {currentItems.map(item => (
+                            <ProductCard
+                                key={item.id}
+                                id={item.id}
+                                product={item}
+                                name={item.nameProduct}
+                                src={item.imageDepan}
+                                description={item.description}
+                                price={`IDR ${item.priceProduct}`}
+                            />
+                        ))}
                     </div>
 
                     {/* Pagination */}
                     <div className="flex justify-center items-center gap-3 mt-12">
-                        <button className="w-10 h-10 rounded-full bg-orange-500 text-white">
-                            1
-                        </button>
-
-                        <button className="w-10 h-10 rounded-full bg-gray-200 text-gray-600">
-                            2
-                        </button>
-
-                        <button className="w-10 h-10 rounded-full bg-gray-200 text-gray-600">
-                            3
-                        </button>
-
-                        <button className="w-10 h-10 rounded-full bg-gray-200 text-gray-600">
-                            4
-                        </button>
-
-                        <button className="w-10 h-10 rounded-full bg-orange-500 text-white">
-                            →
-                        </button>
+                        {Array.from({ length: totalPages }).map((_, index) => (
+                            <button
+                                key={index}
+                                onClick={() => setCurrentPage(index + 1)}
+                                className={`w-10 h-10 rounded-full ${currentPage === index + 1
+                                        ? "bg-orange-500 text-white"
+                                        : "bg-gray-200 text-gray-600"
+                                    }`}
+                            >
+                                {index + 1}
+                            </button>
+                        ))}
                     </div>
                 </section>
             </section>

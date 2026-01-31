e2ce5665d6057da0a81aa63f5de8c2ee52173010
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-31 13:54:02 +0700

    feat: adding id at the route url for displaying product by id which is primary key of the data

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <ForgotPassPage/>
     },
     {
-        path: "/details-product",
+        path: "/details-product/:id",
         element: <DetailProductPage/>
     },
     {

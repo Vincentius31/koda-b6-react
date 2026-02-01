69e58351c699f1c6bee4b415a9f2e24d3404760a
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-02-02 03:32:23 +0700

    fix: adding props ...rest for the value and make the onChange manual

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,15 @@
-export default function Input({ label, type, placeholder, icon: Icon, register, error }) {
+export default function Input({ label, type, placeholder, icon: Icon, register, error, ...rest }) {
     return (
         <div className="form-group">
             <label className="block text-sm mb-2">{label}</label>
             <div className="input-box flex items-center border rounded-md px-3 py-3">
-                {Icon && <Icon className="w-5 mr-3"/>}
+                {Icon && <Icon className="w-5 mr-3 text-gray-400"/>}
                 <input
                     type={type}
                     placeholder={placeholder}
-                    className="w-full outline-none border-none"
+                    className="w-full outline-none border-none bg-transparent"
                     {...register}
+                    {...rest}    
                 />
             </div>
 

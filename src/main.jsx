884e9e5c52f77c3ed931bcda4323d602a32175f1
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-02-01 22:56:34 +0700

    feat: adding authprovider for make the context working

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
 
-
-root.render(<App/>)
+root.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);

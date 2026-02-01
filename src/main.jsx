cc8679c3a61a1c6d8ad234d1b5b7628f2ee72b8e
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-02-02 01:26:37 +0700

    feat: adding CartProvider for the ProductCard

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
 
 root.render(
   <AuthProvider>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </AuthProvider>
 );

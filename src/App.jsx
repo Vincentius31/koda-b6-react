5bbe73b41d17d54983f724813590bdff0fcc5ab3
Vincentius <vincentius85@gmail.com>
2026-02-07 22:53:53 +0700

    fix: fixing the router for the page flow

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AdminLayout from './pages/admin/AdminLayout'
 import Dashboard from './pages/admin/Dashboard'
 import Product from './pages/admin/Product'
 import Order from './pages/admin/Order'
+import TestUser from './components/TestUser'
 
 const router = createBrowserRouter([
     {
@@ -74,6 +75,7 @@ const router = createBrowserRouter([
             }
         ]
     }
+
 ])
 
 export default function App() {

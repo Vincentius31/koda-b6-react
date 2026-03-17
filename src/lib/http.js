8d977143e9c2dfc91b9acb2567b95cd14c4d8782
Vincentius <vincentius85@gmail.com>
2026-03-17 13:26:21 +0700

    fix: fixing the logic

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,18 +1,20 @@
 const BASE_URL = "https://vincent-backend.camps.fahrul.id"
 
-function http(url, opts={}){
+async function http(url, opts={}){ 
     const headers = {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
     }
     if(opts.token){
-        headers.Authorization = "Bearer" + opts.token
+        headers.Authorization = "Bearer " + opts.token
     }
-    return fetch(BASE_URL + url, {
+
+    const response = await fetch(BASE_URL + url, {
         method: opts.method || "GET",
         headers: headers,
-        body: opts.body ? JSON.stringify(opts.body) : undefined,
+        body: opts.body ? JSON.stringify(opts.body) : undefined
     })
-    return Response.json()
+
+    return await response.json()
 }
 
 export default http;

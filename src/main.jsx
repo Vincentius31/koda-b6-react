c2abcb0ac1a012c39fbde0e7dd61a34abb54a000
Vincentius <vincentius85@gmail.com>
2026-03-29 02:33:12 +0700

    fix: remove redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { CartProvider } from "./context/CartContext";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./components/redux/store";
+import { store} from "./components/redux/store";
 
 const el = document.getElementById("root")
 const root = createRoot(el)
 
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </PersistGate>
+    <App />
   </Provider>
 )
